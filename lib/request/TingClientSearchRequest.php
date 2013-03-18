e8736b6b5ae2de42d87a4b7e2cd8b7963458923e
Jesper Kristensen <cableman@linuxdev.dk>
2013-03-18 16:22:58 +0100

    DBC responces sometimes return XML where not all namespaces are defined.
    This if statement checks if the namespace exists as trying to access the array with a not defined namespace will give a PHP notice.
    
    Bug report have been sent to DBC.

--- lib/request/TingClientSearchRequest.php
+++ lib/request/TingClientSearchRequest.php
@@ -253,9 +253,12 @@ class TingClientSearchRequest extends TingClientRequest {
           $key1 = $prefix . ':' . $name;
           if (isset($element->{'@type'}) && strpos($element->{'@type'}->{'$'}, ':') !== FALSE) {
             list($type_prefix, $type_name) = explode(':', $element->{'@type'}->{'$'}, 2);
-            $type_namespace = $namespaces[isset($type_prefix) ? $type_prefix : '$'];
-            $type_prefix = isset($prefixes[$type_namespace]) ? $prefixes[$type_namespace] : 'unknown';
-            $key2 = $type_prefix . ':' . $type_name;
+            $key2 = '';
+            if (isset($namespaces[isset($type_prefix) ? $type_prefix : '$'])) {
+              $type_namespace = $namespaces[isset($type_prefix) ? $type_prefix : '$'];
+              $type_prefix = isset($prefixes[$type_namespace]) ? $prefixes[$type_namespace] : 'unknown';
+              $key2 = $type_prefix . ':' . $type_name;
+            }
           }
           else {
             $key2 = '';
