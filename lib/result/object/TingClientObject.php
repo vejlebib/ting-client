0da37c99b448825bd10bfbe364ebe77ff642a3a3
Jesper Kristensen <cableman@linuxdev.dk>
2014-05-28 15:51:25 +0200

    Fixed indention in ting client object

--- lib/result/object/TingClientObject.php
+++ lib/result/object/TingClientObject.php
@@ -4,12 +4,10 @@ class TingClientObject {
   public $id;
   public $data;
 
-
   /**
-  * Return object type.
-  **/
+   * Return object type.
+   **/
   public function getType() {
-    return (string)$this->record['dc:type']['dkdcplus:BibDK-Type'][0];
+    return (string) $this->record['dc:type']['dkdcplus:BibDK-Type'][0];
   }
 }
-
