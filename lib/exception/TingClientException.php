fd5df70b1c14dff703fa9c7c22b99fedb28ee884
pjohans <pjohans@gmail.com>
2015-06-17 12:52:20 +0200

    Issue #429 by pjohans: pass different errormessages for different situations

--- lib/exception/TingClientException.php
+++ lib/exception/TingClientException.php
@@ -2,9 +2,9 @@
 
 class TingClientException extends Exception {
   public function __construct($message, $code = 0, Exception $previous = null) {
-    foreach($this->errors as $key => $val){
-      if(strpos($message, $key) !== FALSE){
-        drupal_set_message(t($this->errors[$key].': '. $message), 'warning');
+    foreach($this->errors as $val){
+      if(strpos($message, $val) !== FALSE){
+        drupal_set_message(t($val), 'warning');
         break;
       }
     }
@@ -12,10 +12,10 @@ class TingClientException extends Exception {
   }
 
   private $errors = array(
-    'Unsupported index' => 'Fix your syntax',
-    'Unsupported boolean modifier' => 'Fix your syntax',
-    'Invalid or unsupported use' => 'Fix ypur syntax',
-    'Internal problem' => 'Try agoin later',
+    'Unsupported index',
+    'Unsupported boolean modifier',
+    'Invalid or unsupported use',
+    'Internal problem',
   );
 }
 
